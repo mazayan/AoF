b25e9feec913b6c4e7f141f982b0406d38f145d0
Marilynn Zayan <maz5156@psu.edu>
2018-01-02 20:09:30 -0500

    Bash file changes

--- main.sh
+++ main.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
 
-#stop of first error
+#stop on first error
 set -e
 
 #CONFIG
-readonly DATASET1_PATH="~/data/"
-readonly DATASET2_PATH="~/data/County_Dataset.csv"
-readonly VENV_PATH=""
+#readonly DATASET1_PATH="/usr/bin/env/AoF_Project/data/"
+#readonly DATASET2_PATH="/usr/bin/env/AoF_Project/data/County_Dataset.csv"
+#readonly VENV_PATH="/usr/bin/env"
+
+readonly DATASET1_PATH="~/env/data/"
+readonly DATASET2_PATH="~/env/data/County_Dataset.csv"
+readonly VENV_PATH="~/env"
+
+python3 -m venv /Users/Mazayan/Desktop
 #END CONFIG
 
 
 main() {
-    if [-d $VENV_PATH ]; then
+
+    git clone....
+
+    if [ -d $VENV_PATH ]; then
         echo "Skipping venv creation as it already exists."
     else
         echo "Creating venv..."
-        virtualenv create -r requirements.txt $VENV_PATH
+        #virtualenv create -r requirements.txt $VENV_PATH
+        python3 -m virtualenv $VENV_PATH
+        pip3 install -r requirements.txt
         echo "Done Creating venv"
     fi
 
@@ -23,7 +34,7 @@ main() {
     source $VENV_PATH/activate
     echo "In venv"
 
-    if [-f $DATASET1_PATH ]; then
+    if [ -f $DATASET1_PATH ]; then
         echo "Skipping download since opioid dataset exists"
     else
         echo "Downloading opioid dataset..."
